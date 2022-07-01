4316189de7c3950aa2a9eece779638249238cc25
Yurun <admin@yurunsoft.com>
2022-07-01 16:15:04 +0800

    适配 Swoole 5.0 新的 pgsql 客户端 (#370)

---
+++ src/Db/Drivers/SwooleNew/Statement.php
@@ -0,0 +1,418 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Pgsql\Db\Drivers\SwooleNew;
+
+use Imi\Db\Exception\DbException;
+use Imi\Pgsql\Db\Contract\IPgsqlDb;
+use Imi\Pgsql\Db\Contract\IPgsqlStatement;
+use Imi\Pgsql\Db\PgsqlBaseStatement;
+use Imi\Swoole\Util\Coroutine;
+use Imi\Util\Text;
+use Swoole\Coroutine\PostgreSQLStatement;
+
+/**
+ * Swoole Coroutine Pgsql 驱动 Statement.
+ */
+class Statement extends PgsqlBaseStatement implements IPgsqlStatement
+{
+    protected ?PostgreSQLStatement $stmt = null;
+
+    /**
+     * 数据库操作对象
+     */
+    protected ?IPgsqlDb $db = null;
+
+    /**
+     * 绑定数据.
+     */
+    protected array $bindValues = [];
+
+    /**
+     * 结果数组.
+     */
+    protected array $result = [];
+
+    /**
+     * 最后执行过的SQL语句.
+     */
+    protected string $lastSql = '';
+
+    /**
+     * SQL 参数映射.
+     */
+    protected ?array $sqlParamsMap = null;
+
+    /**
+     * 最后插入ID.
+     */
+    protected string $lastInsertId = '';
+
+    public function __construct(IPgsqlDb $db, PostgreSQLStatement $stmt, string $originSql, ?array $sqlParamsMap = null, bool $isExecuted = false)
+    {
+        $this->db = $db;
+        $this->stmt = $stmt;
+        $this->lastSql = $originSql;
+        $this->sqlParamsMap = $sqlParamsMap;
+        if ($isExecuted)
+        {
+            if ($result = $stmt->fetchAll(\SW_PGSQL_ASSOC))
+            {
+                $this->result = $result;
+            }
+            $this->updateLastInsertId();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDb(): IPgsqlDb
+    {
+        return $this->db;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function bindColumn($column, &$param, ?int $type = null, ?int $maxLen = 0, $driverData = null): bool
+    {
+        $this->bindValues[$column] = $param;
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function bindParam($parameter, &$variable, int $dataType = \PDO::PARAM_STR, ?int $length = 0, $driverOptions = null): bool
+    {
+        $this->bindValues[$parameter] = $variable;
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function bindValue($parameter, $value, int $dataType = \PDO::PARAM_STR): bool
+    {
+        if (\is_int($parameter))
+        {
+            --$parameter;
+        }
+        $this->bindValues[$parameter] = $this->parseValue($value);
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function closeCursor(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function columnCount(): int
+    {
+        return \count($this->result[0] ?? []);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function errorCode()
+    {
+        if ($this->stmt->resultDiag)
+        {
+            return $this->stmt->resultDiag['sqlstate'] ?? null;
+        }
+        else
+        {
+            return '';
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function errorInfo(): string
+    {
+        return $this->stmt->error ?? '';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSql(): string
+    {
+        return $this->lastSql;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function execute(array $inputParameters = null): bool
+    {
+        if (null === $inputParameters)
+        {
+            $inputParameters = $this->bindValues;
+        }
+        $this->bindValues = $bindValues = [];
+        if ($inputParameters)
+        {
+            $sqlParamsMap = $this->sqlParamsMap;
+            if ($sqlParamsMap)
+            {
+                foreach ($sqlParamsMap as $index => $paramName)
+                {
+                    if (isset($inputParameters[$paramName]))
+                    {
+                        $bindValues[$index] = $this->parseValue($inputParameters[$paramName]);
+                    }
+                    elseif (isset($inputParameters[$key = ':' . $paramName]))
+                    {
+                        $bindValues[$index] = $this->parseValue($inputParameters[$key]);
+                    }
+                    elseif (isset($inputParameters[$index]))
+                    {
+                        $bindValues[$index] = $this->parseValue($inputParameters[$index]);
+                    }
+                }
+            }
+            else
+            {
+                foreach ($inputParameters as $value)
+                {
+                    $bindValues[] = $this->parseValue($value);
+                }
+            }
+        }
+        $stmt = $this->stmt;
+        if (false === $stmt->execute($bindValues))
+        {
+            $errorCode = $this->errorCode();
+            $errorInfo = $this->errorInfo();
+            if ($this->db->checkCodeIsOffline($errorCode))
+            {
+                $this->db->close();
+            }
+            throw new DbException('SQL query error: [' . $errorCode . '] ' . $errorInfo . \PHP_EOL . 'sql: ' . $this->getSql() . \PHP_EOL);
+        }
+        $this->updateLastInsertId();
+        $this->result = $stmt->fetchAll(\SW_PGSQL_ASSOC) ?: [];
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function fetch(int $fetchStyle = \PDO::FETCH_ASSOC, int $cursorOrientation = \PDO::FETCH_ORI_NEXT, int $cursorOffset = 0)
+    {
+        $result = current($this->result);
+        if ($result)
+        {
+            next($this->result);
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function fetchAll(int $fetchStyle = \PDO::FETCH_ASSOC, $fetchArgument = null, array $ctorArgs = []): array
+    {
+        return $this->result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function fetchColumn($columnKey = 0)
+    {
+        $row = current($this->result);
+        if ($row)
+        {
+            next($this->result);
+            if (isset($row[$columnKey]))
+            {
+                return $row[$columnKey];
+            }
+            elseif (is_numeric($columnKey))
+            {
+                return array_values($row)[$columnKey] ?? null;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function fetchObject(string $className = \stdClass::class, ?array $ctorArgs = null)
+    {
+        $row = current($this->result);
+        if (false === $row)
+        {
+            return null;
+        }
+        next($this->result);
+        if (\stdClass::class === $className)
+        {
+            return (object) $row;
+        }
+        $result = new $className();
+        foreach ($row as $k => $v)
+        {
+            $result->{$k} = $v;
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAttribute($attribute)
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setAttribute($attribute, $value): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function nextRowset(): bool
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function lastInsertId(?string $name = null): string
+    {
+        if (null === $name)
+        {
+            return $this->lastInsertId;
+        }
+        else
+        {
+            return $this->db->lastInsertId($name);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rowCount(): int
+    {
+        return $this->stmt->affectedRows();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstance()
+    {
+        return $this;
+    }
+
+    /**
+     * @return mixed|false
+     */
+    #[\ReturnTypeWillChange]
+    public function current()
+    {
+        return current($this->result);
+    }
+
+    /**
+     * @return int|string|null
+     */
+    #[\ReturnTypeWillChange]
+    public function key()
+    {
+        return key($this->result);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function next(): void
+    {
+        next($this->result);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rewind(): void
+    {
+        reset($this->result);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function valid(): bool
+    {
+        return false !== $this->current();
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    protected function parseValue($value)
+    {
+        if (\is_bool($value))
+        {
+            return (int) $value;
+        }
+
+        return $value;
+    }
+
+    /**
+     * 更新最后插入ID.
+     */
+    private function updateLastInsertId(): void
+    {
+        $queryString = $this->lastSql;
+        if (Text::startwith($queryString, 'insert ', false) || Text::startwith($queryString, 'replace ', false))
+        {
+            try
+            {
+                $this->lastInsertId = $this->db->lastInsertId();
+            }
+            catch (\Throwable $th)
+            {
+                if (!str_contains($th->getMessage(), 'lastval is not yet defined in this session'))
+                {
+                    throw $th;
+                }
+            }
+        }
+        else
+        {
+            $this->lastInsertId = '';
+        }
+    }
+}
